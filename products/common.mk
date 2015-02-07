9ab61e18b8e123b08f4e7ebd3b25b6580991a7a0
zephiK <zephik@gmail.com>
2015-02-06 21:05:51 -0500

    Add CDMA APNs

--- products/common.mk
+++ products/common.mk
@@ -40,7 +40,7 @@ PRODUCT_PACKAGES += \
 endif
 
 # Block based ota flag default to off to get old style ota zip back (To get back block based zip, just enable to true.)
-TARGET_USES_BLOCK_BASED_OTA := false
+#TARGET_USES_BLOCK_BASED_OTA := true
 
 # Latin IME lib - gesture typing
 PRODUCT_COPY_FILES += \
@@ -52,7 +52,8 @@ PRODUCT_PACKAGES += \
 
 # APN list
 PRODUCT_COPY_FILES += \
-    vendor/chroma/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/chroma/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/chroma/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf-cdma.xml
 
 # SuperSU
 PRODUCT_COPY_FILES += \
