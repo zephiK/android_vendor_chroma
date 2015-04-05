a5a8246d84dd308b2745d44e5f132e2a21d7c173
sykopompos <sykopompos@gmail.com>
2015-04-05 14:21:01 -0400

    add manta, flo and deb products if we choose to make them also remove vold sepolicy, grouper will fail building on that so we will have to move vold lines to vendor specific

--- products/common.mk
+++ products/common.mk
@@ -77,6 +77,23 @@ LOCAL_PATH := $(call vendor/chroma/)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 #This is because syko doen't like having to open 20 products just to change numbers
+
+ifeq ($(TARGET_PRODUCT),chroma_deb)
+# FIngerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=razorg \
+BUILD_FINGERPRINT=google/razorg/deb:5.1/LMY47I/1767468:user/release-keys \
+PRIVATE_BUILD_DESC="razorg-user 5.1 LMY47I 1767468 release-keys"
+endif
+
+ifeq ($(TARGET_PRODUCT),chroma_flo)
+# FIngerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=razor \
+BUILD_FINGERPRINT=google/razor/flo:5.1/LMY47I/1767468:user/release-keys \
+PRIVATE_BUILD_DESC="razor-user 5.1 LMY47I 1767468 release-keys"
+endif
+
 ifeq ($(TARGET_PRODUCT),chroma_grouper)
 # FIngerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -99,6 +116,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
     BUILD_FINGERPRINT=google/occam/mako:5.1/LMY47I/1767468:user/release-keys \
     PRIVATE_BUILD_DESC="occam-user 5.1 LMY47I 1767468 release-keys"
+endif
+
+ifeq ($(TARGET_PRODUCT),chroma_manta)
+# FIngerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mantaray \
+    BUILD_FINGERPRINT=google/mantaray/manta:5.1/LMY47I/1767468:user/release-keys \
+    PRIVATE_BUILD_DESC="mantaray-user 5.1 LMY47I 1767468 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_shamu)
