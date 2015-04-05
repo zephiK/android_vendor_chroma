a8ecd5b2e4724448ccf92b29e08eb1a249e21957
sykopompos <sykopompos@gmail.com>
2015-04-04 22:36:34 -0400

    Turn art optimization to everything by default, art optimization is set to speed : speed - compiles most methods and maximizes runtime performance but we are changing to everything here : everything - compiles almost everything, excluding class initializers and some rare methods that are too large to be represented by the compiler’s internal representation.
    art is a different beast then dalvik and by creating oat files with more in them we are speeding up loading
    this creates a trade off of a minute or 2 optimizing apps at boot for quicker and steadier performance
    
    Make sure users wipe thier cahce after first flashing like this to force creation of new oat files

--- products/common.mk
+++ products/common.mk
@@ -24,6 +24,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.dun.override=0 \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
+    dalvik.vm.image-dex2oat-filter=everything \
+    dalvik.vm.dex2oat-filter=everything
     
 # Launcher3 supported devices
 PRODUCT_PACKAGES += \
