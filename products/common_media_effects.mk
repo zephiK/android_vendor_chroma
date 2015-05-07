c3425bda70de3af5575d9ffb93856b5880cd7235
zephiK <zephik@gmail.com>
2015-05-07 14:28:44 -0400

    Remove obsolete common mkfile In commit, https://github.com/zephiK/android_frameworks_base/commit/db8c1655ea4af060967cdaef9c1bc74a1003cdc8 we remove the obsolete assets and the makefile is not making anything, so there is no need for this in .mk

--- products/common_media_effects.mk
+++ products/common_media_effects.mk
@@ -4,6 +4,3 @@
 PRODUCT_COPY_FILES +=  \
     vendor/chroma/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/chroma/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
-
-# video files
-$(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackage2.mk)
