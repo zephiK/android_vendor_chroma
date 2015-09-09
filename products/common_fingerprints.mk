8d7bd1b5c86f482032076c477a1373b5f18f7b4b
zephiK <zephik@gmail.com>
2015-09-08 21:22:39 -0400

    Update to LMY48M (_r14) fingerprints

--- products/common_fingerprints.mk
+++ products/common_fingerprints.mk
@@ -2,22 +2,22 @@ ifeq ($(TARGET_PRODUCT),chroma_hammerhead)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LYZ28J/2128181:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LYZ28J 2128181 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LMY48M/2128181:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48M 2128181 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_mako)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LYZ28J/2128181:user/release-keys \
-    PRIVATE_BUILD_DESC="occam-user 5.1.1 LYZ28J 2128181 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2128181:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2128181 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_shamu)
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LYZ28J/2128181:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28J 2128181 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LMY48M/2128181:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LMY48M 2128181 release-keys"
 endif
