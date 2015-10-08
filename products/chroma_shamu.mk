6fb48148b19f241e70a56e9597783fa0e92408ec
zephiK <zephik@gmail.com>
2015-10-08 18:36:11 -0400

    Remove duplicate fingerprint Forgot to remove this as I was messing around with the fingerprints

--- products/chroma_shamu.mk
+++ products/chroma_shamu.mk
@@ -14,9 +14,3 @@ PRODUCT_MANUFACTURER := motorola
 # Boot animation
 PRODUCT_COPY_FILES += \
     vendor/chroma/prebuilt/shamu/system/media/bootanimation.zip:system/media/bootanimation.zip
-
-# Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
