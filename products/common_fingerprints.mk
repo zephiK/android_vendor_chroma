99c64b8115f9508fe71491f165ba8216e7eea53f
zephiK <zephik@gmail.com>
2015-11-06 01:09:07 -0500

    Update fingerprints Update according to the factory image names

--- products/common_fingerprints.mk
+++ products/common_fingerprints.mk
@@ -1,5 +1,4 @@
 ifeq ($(TARGET_PRODUCT),chroma_angler)
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT=google/angler/angler:6.0/MDB08M/2353240:user/release-keys \
@@ -7,7 +6,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_bullhead)
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT=google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys \
@@ -15,15 +13,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_hammerhead)
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58V/2324235:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58V 2324235 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58N/2324235:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58N 2324235 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_mako)
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
     BUILD_FINGERPRINT=google/occam/mako:6.0/MRA58V/2324235:user/release-keys \
@@ -31,10 +27,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_shamu)
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58V/2324235:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58V 2324235 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58R/2324235:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58R 2324235 release-keys"
 endif
 
