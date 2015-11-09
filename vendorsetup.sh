1618ed279ac0677238bf747587f1300fabde3b47
zephiK <zephik@gmail.com>
2015-11-08 19:14:11 -0500

    angler: user -> userdebug

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo chroma_angler-user
+add_lunch_combo chroma_angler-userdebug
 add_lunch_combo chroma_bullhead-userdebug
 add_lunch_combo chroma_hammerhead-userdebug
 add_lunch_combo chroma_mako-userdebug
