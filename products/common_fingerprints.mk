da72c21580f294efff3d5dc48e19e1030322e4b6
zephiK <zephik@gmail.com>
2015-12-09 20:16:04 -0500

    angler: update fingerprint incremental number

--- products/common_fingerprints.mk
+++ products/common_fingerprints.mk
@@ -1,8 +1,8 @@
 ifeq ($(TARGET_PRODUCT),chroma_angler)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2419427:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2419427 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_bullhead)
