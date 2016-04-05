321e2a23eaacadc271f221d24194b8ab9238f6a0
zephiK <zephik@gmail.com>
2016-04-05 02:37:45 -0400

    Update Fingerprints (MHC19Q/MMB29X)

--- products/common_fingerprints.mk
+++ products/common_fingerprints.mk
@@ -1,35 +1,35 @@
 ifeq ($(TARGET_PRODUCT),chroma_angler)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19I/2590160:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19I 2590160 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19Q/2705526:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19Q 2705526 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_bullhead)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MHC19J/2595691:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19J 2595691 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MHC19Q/2705526:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19Q 2705526 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_hammerhead)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29X/2704508:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29X 2704508 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_mako)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="occam-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29X/2704508:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 6.0.1 MMB29X 2704508 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),chroma_shamu)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MMB29X/2704508:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29X 2704508 release-keys"
 endif
 
